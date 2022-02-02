ca0157ded548966ebba5a6cf376952bc9c36d9a8
litvinenkoiryna <litvinenkoiryna@gmail.com>
2022-02-02 19:37:15 +0200

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 48.720762334393584, lng: 37.555464244293496 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+let anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+            })
+      })
+}
+
+
+
+    $(document).ready(function(){
+    $('.news__carusel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        dots: true
+    });
+});
+
+
